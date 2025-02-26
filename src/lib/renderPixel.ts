4f186a4020c101c75c3e53ff987d69514c88f4e5
맹주영 <mjy9088@naver.com>
2025-02-26 09:27:52 +0900

    refactor

--- src/lib/renderPixel.ts
+++ src/lib/renderPixel.ts
@@ -1,8 +1,24 @@
 import { Pixel } from "../common/types";
 import { Context } from "./Context";
 import { scene } from "./scene";
-import { Color, Ray } from "./types";
+import { Color, Intersection, Ray, RTObject } from "./types";
 import { add, c, dot, len, mul, normalize, sub, v } from "./util";
+
+function intersect(ray: Ray): [Intersection, RTObject] | undefined {
+  let result: [Intersection, RTObject] | undefined;
+
+  for (const object of scene.objects) {
+    const intersection = object.intersect(ray);
+    if (
+      intersection.length &&
+      (!result || intersection[0].distance < result[0].distance)
+    ) {
+      result = [intersection[0], object];
+    }
+  }
+
+  return result;
+}
 
 export function renderPixel(context: Context, x: number, y: number): Pixel {
   const ray: Ray = {
@@ -12,49 +28,35 @@ export function renderPixel(context: Context, x: number, y: number): Pixel {
     ),
   };
 
-  let distance = Infinity;
-  let color = c(0, 0, 0);
-  for (const object of scene.objects) {
-    const intersection = object.intersect(ray)[0];
-    if (intersection && intersection.distance < distance) {
-      distance = intersection.distance;
-      let sum: Color = {
-        r: scene.ambientLight.r * intersection.color.r,
-        g: scene.ambientLight.g * intersection.color.g,
-        b: scene.ambientLight.b * intersection.color.b,
-      };
-      const point = add(
-        add(ray.origin, mul(ray.direction, intersection.distance)),
-        mul(intersection.normal, 0.0001)
+  const hit = intersect(ray);
+  if (!hit) {
+    return c(0, 0, 0);
+  }
+  const [intersection, object] = hit;
+  let sum: Color = {
+    r: scene.ambientLight.r * intersection.color.r,
+    g: scene.ambientLight.g * intersection.color.g,
+    b: scene.ambientLight.b * intersection.color.b,
+  };
+  const point = add(
+    add(ray.origin, mul(ray.direction, intersection.distance)),
+    mul(intersection.normal, 0.0001)
+  );
+  for (const light of scene.lights) {
+    const pointToLight = sub(light.position, point);
+    const ray: Ray = { origin: point, direction: normalize(pointToLight) };
+    const distance = intersect(ray)?.[0].distance ?? Infinity;
+    if (distance > len(pointToLight)) {
+      const factor = Math.max(
+        0,
+        dot(intersection.normal, normalize(pointToLight))
       );
-      for (const light of scene.lights) {
-        const pointToLight = sub(light.position, point);
-        const ray: Ray = { origin: point, direction: normalize(pointToLight) };
-        let distance = Infinity;
-        for (const object of scene.objects) {
-          const intersection = object.intersect(ray)[0];
-          if (intersection && intersection.distance < distance) {
-            distance = intersection.distance;
-            if (distance < len(pointToLight)) {
-              break;
-            }
-          }
-        }
-        if (distance > len(pointToLight)) {
-          const factor = Math.max(
-            0,
-            dot(intersection.normal, normalize(pointToLight))
-          );
-          sum = {
-            r: sum.r + light.color.r * intersection.color.r * factor,
-            g: sum.g + light.color.g * intersection.color.g * factor,
-            b: sum.b + light.color.b * intersection.color.b * factor,
-          };
-        }
-      }
-      color = sum;
+      sum = {
+        r: sum.r + light.color.r * intersection.color.r * factor,
+        g: sum.g + light.color.g * intersection.color.g * factor,
+        b: sum.b + light.color.b * intersection.color.b * factor,
+      };
     }
   }
-
-  return color;
+  return sum;
 }
