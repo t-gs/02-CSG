181b438a92d525a03be6253f655c8bb8bd50fa0f
Juyeong Maing <mjy9088@naver.com>
2025-03-10 23:32:19 +0900

    add cumulative changes

--- src/lib/scene.ts
+++ src/lib/scene.ts
@@ -1,3 +1,4 @@
+import { dummyTexture } from "./assets/dummyTexture";
 import { CSGDifference } from "./CSGDifference";
 import { CSGIntersection } from "./CSGIntersection";
 import { Plane } from "./Plane";
@@ -7,15 +8,15 @@ import { c, v } from "./util";
 
 export const scene: Scene = {
   objects: [
-    new Plane(v(0, 0, -5), v(0, 0, 1), c(1, 1, 1)),
+    new Plane(v(0, 0, -6), v(0, 0, 1), c(1, 1, 1)),
     new CSGIntersection(
       new CSGDifference(
-        new Sphere(v(-4, 10, 0), 2, c(1, 0, 0)),
-        new Sphere(v(-2, 10, 0), 2, c(0, 1, 0))
+        new Sphere(v(-5, 10, 0), 2, c(1, 0, 0)),
+        new Sphere(v(-3, 10, 0), 2, c(0, 0, 1), dummyTexture)
       ),
-      new Plane(v(-3.3, 0, 0), v(1, 0, 0), c(0, 0, 1))
+      new Plane(v(-4.3, 0, 0), v(1, 0, 0), c(0, 1, 0))
     ),
-    new Sphere(v(4, 10, 0), 2, c(0.5, 0.5, 0.5)),
+    new Sphere(v(5, 10, 0), 2, c(0.5, 0.5, 0.5)),
   ],
   lights: [
     { position: v(0, 10, 10), color: c(0.4, 0.4, 0.4) },
